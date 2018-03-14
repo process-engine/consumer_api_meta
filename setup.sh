8687e38d106d12bcb794058619361792d00e0903
ElRaptorus <christian.werner@5minds.de>
2018-03-14 17:15:10 +0100

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ npm install -g meta gulp
 
 # checkout all repos in the correct branch
 meta git update
-meta exec "git checkout develop" --exclude process_engine_meta
+meta exec "git checkout feature/apply_consumer_api_concept"
 
 # retrieve latest versions
 meta git pull
@@ -11,12 +11,8 @@ meta git pull
 # install all necessary dependencies
 npm install --no-package-lock
 
-# fix conflicting types from jasmine and mocha
-rm -rf node_modules/@types/jasmine
-
 # build all packages and schemas
-meta exec "npm run build-schemas && npm run build" --exclude process_engine_meta,skeleton,documentation,bpmn-studio
-meta exec "npm run build" --include-only skeleton,bpmn-studio
+meta exec "npm run build" --exclude consumer_api_meta
 
 # create a database
 cd skeleton/database
@@ -24,5 +20,5 @@ node postgres_docker.js reset demo
 cd ../..
 
 # tell the user how to run stuff
-echo "run 'npm start' in 'skeleton/process-engine-server-demo' to run the process-engine"
-echo "run 'npm start' in 'bpmn-studio' to run the frontend"
+# echo "run 'npm start' in 'skeleton/process-engine-server-demo' to run the process-engine"
+# echo "run 'npm start' in 'bpmn-studio' to run the frontend"
