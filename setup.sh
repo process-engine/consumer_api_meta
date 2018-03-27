78deec2c6fd0da864b8e2fd3e0adf8178450eaec
Heiko Mathes <heiko.mathes@gmail.com>
2018-03-27 11:48:27 +0200

    :wrench: update setup to reference user_tasks-branches

--- setup.sh
+++ setup.sh
@@ -3,8 +3,7 @@ npm install -g meta gulp
 
 # checkout all repos in the correct branch
 meta git update
-meta exec "git checkout feature/apply_consumer_api_concept" --exclude consumer_api_meta,consumer_api_client,consumer_api_contracts
-meta exec "git checkout feature/user_tasks" --include-only consumer_api_core
+meta exec "git checkout feature/user_tasks"
 
 # retrieve latest versions
 meta git pull
