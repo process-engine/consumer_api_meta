730d6bfd097e25bbe9b28cda4f0b8326041bb82d
ElRaptorus <christian.werner@5minds.de>
2018-05-08 10:03:06 +0200

    Add reinstall script which only installs and builds

---
+++ reinstall.sh
@@ -0,0 +1,10 @@
+# remove "node_modules"
+echo "Purging node_modules..."
+find . -name "node_modules" -exec rm -rf '{}' +
+echo "Done. Starting setup..."
+
+# install all necessary dependencies
+npm install --no-package-lock
+
+# build all packages and schemas
+meta exec "npm run build" --exclude consumer_api_meta
