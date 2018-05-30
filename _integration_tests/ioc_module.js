1aac84a2e3c091dbefacadc216174a10d5767421
robpal94 <robin.palkovits@5minds.de>
2018-05-30 09:27:35 +0200

    :fire: remove unecessary test process

--- _integration_tests/ioc_module.js
+++ _integration_tests/ioc_module.js
@@ -11,7 +11,6 @@ const registerInContainer = (container) => {
     'test_consumer_api_non_executable_process',
     'test_consumer_api_process_start',
     'test_consumer_api_usertask',
-    'test_consumer_api_sublane_normal_process',
     'test_consumer_api_sublanes_multiple_sublanes',
   ];
 
