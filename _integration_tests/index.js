267bbccf332228171bacd3ccd48b76420ffe4c1b
ElRaptorus <christian.werner@5minds.de>
2018-07-24 15:35:20 +0200

    Cleanup index file

--- _integration_tests/index.js
+++ _integration_tests/index.js
@@ -7,37 +7,16 @@ const path = require('path');
 const iocModuleNames = [
   '@essential-projects/bootstrapper',
   '@essential-projects/bootstrapper_node',
-  '@essential-projects/caching',
-  '@essential-projects/core',
-  '@essential-projects/data_model',
-  '@essential-projects/data_model_contracts',
-  '@essential-projects/datasource_adapter_base',
-  '@essential-projects/datasource_adapter_postgres',
-  '@essential-projects/datastore',
-  '@essential-projects/datastore_http',
-  '@essential-projects/datastore_messagebus',
   '@essential-projects/event_aggregator',
-  '@essential-projects/feature',
   '@essential-projects/http_extension',
-  '@essential-projects/http_integration_testing',
-  '@essential-projects/iam',
-  '@essential-projects/iam_http',
-  '@essential-projects/invocation',
-  '@essential-projects/messagebus',
-  '@essential-projects/messagebus_http',
-  '@essential-projects/messagebus_adapter_faye',
-  '@essential-projects/metadata',
-  '@essential-projects/pki_service',
-  '@essential-projects/security_service',
   '@essential-projects/services',
-  '@essential-projects/routing',
-  '@essential-projects/timing',
-  '@essential-projects/validation',
   '@process-engine/consumer_api_core',
   '@process-engine/consumer_api_http',
+  '@process-engine/flow_node_instance.repository.sequelize',
+  '@process-engine/iam',
   '@process-engine/process_engine',
-  '@process-engine/process_engine_http',
-  '@process-engine/process_repository',
+  '@process-engine/process_model.repository.sequelize',
+  '@process-engine/timers.repository.sequelize',
   '.',
 ];
 
@@ -48,7 +27,7 @@ const iocModules = iocModuleNames.map((moduleName) => {
 let container;
 
 // NOTE: This startup script allows for the usage of the BPMN studio in conjunction with
-// the integrationtest app, which enables us to edit the integrationtests' bpmn files without having 
+// the integrationtest app, which enables us to edit the integrationtests' bpmn files without having
 // to import them manually.
 async function start() {
 
