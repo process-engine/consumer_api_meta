94cf6f6142dd654de238f2f3547ff2d0801c5568
5ebastianMeier <sebastian.meier@5minds.de>
2018-09-12 11:06:35 +0200

    add test for user task waiting message

---
+++ _integration_tests/test/user_tasks/user_task_waiting_message.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const should = require('should');
+
+const TestFixtureProvider = require('../../dist/commonjs').TestFixtureProvider;
+const ProcessInstanceHandler = require('../../dist/commonjs').ProcessInstanceHandler;
+
+describe('Consumer API:   Receive User Task Waiting Notification', () => {
+
+  let processInstanceHandler;
+  let testFixtureProvider;
+
+  let consumerContext;
+  let correlationId;
+
+  const processModelId = 'test_consumer_api_usertask';
+  const processModelIdNoUserTasks = 'test_consumer_api_usertask_empty';
+
+  before(async () => {
+    testFixtureProvider = new TestFixtureProvider();
+    await testFixtureProvider.initializeAndStart();
+    consumerContext = testFixtureProvider.context.defaultUser;
+
+    const processModelsToImport = [
+      processModelId,
+      processModelIdNoUserTasks,
+    ];
+
+    await testFixtureProvider.importProcessFiles(processModelsToImport);
+
+    processInstanceHandler = new ProcessInstanceHandler(testFixtureProvider);
+  });
+
+  after(async () => {
+    await finishWaitingUserTasksAfterTests();
+    await testFixtureProvider.tearDown();
+  });
+
+  async function finishWaitingUserTasksAfterTests() {
+    const userTaskId = 'Task_1vdwmn1';
+    const userTaskResult = {
+      formFields: {
+        Form_XGSVBgio: true,
+      },
+    };
+
+    await testFixtureProvider
+      .consumerApiClientService
+      .finishUserTask(consumerContext, processModelId, correlationId, userTaskId, userTaskResult);
+  }
+
+  it('should send a notification via socket when user task is suspended', async () => {
+
+    correlationId = await processInstanceHandler.startProcessInstanceAndReturnCorrelationId(processModelId);
+
+    let userTaskWaitingMessage;
+
+    testFixtureProvider.consumerApiClientService.onUserTaskWaiting((message) => {
+      userTaskWaitingMessage = message;
+    });
+
+    await processInstanceHandler.waitForProcessInstanceToReachUserTask(correlationId);
+
+    // const userTaskList = await testFixtureProvider
+    //   .consumerApiClientService
+    //   .getUserTasksForProcessModel(consumerContext, processModelId);
+
+    should(userTaskWaitingMessage).not.be.undefined();
+
+  });
+
+});
