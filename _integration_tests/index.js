363d9bf7e1e51599235094dd1c0f2ad35d481484
ElRaptorus <christian.werner@5minds.de>
2018-10-01 16:47:33 +0200

    :recycle: Use semver versioning

--- _integration_tests/index.js
+++ _integration_tests/index.js
@@ -16,6 +16,8 @@ const iocModuleNames = [
   '@process-engine/correlations.repository.sequelize',
   '@process-engine/flow_node_instance.repository.sequelize',
   '@process-engine/iam',
+  '@process-engine/logging_api_core',
+  '@process-engine/logging.repository.file_system',
   '@process-engine/metrics_api_core',
   '@process-engine/metrics.repository.file_system',
   '@process-engine/process_engine_core',
