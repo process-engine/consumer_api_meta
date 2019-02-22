7224111d24114c0c38ff63098d326e9795dcc9b7
robpal94 <robin.palkovits@5minds.de>
2019-02-22 11:09:04 +0100

    :fire: Remove only from Test Definition

--- _integration_tests/test/process_instances/start_process_and_resolve_on_end_event.js
+++ _integration_tests/test/process_instances/start_process_and_resolve_on_end_event.js
@@ -9,7 +9,7 @@ const TestFixtureProvider = require('../../dist/commonjs').TestFixtureProvider;
 
 // eslint-disable-next-line
 const testCase = 'Consumer API:   POST  ->  /process_models/:process_model_id/start_events/:start_event_id/start?start_callback_type=3&end_event_id=value';
-describe.only(`Consumer API: ${testCase}`, () => {
+describe(`Consumer API: ${testCase}`, () => {
 
   let testFixtureProvider;
   let defaultIdentity;
